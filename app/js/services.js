d75a7e9c63f36032d56687411ae71e5db010e9fe
Justin Levine <jclevine.receipts@gmail.com>
2013-10-02 17:55:04 -0400

    Wired numPlayers dropdown to table row size.

--- app/js/services.js
+++ app/js/services.js
@@ -2,9 +2,6 @@
 
 /* Services */
 
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
 var bgScoreTallyModule = angular.module('BgScoreTally.services', []);
 bgScoreTallyModule.factory("SelectionService", function() {
   var selectionService = {};
