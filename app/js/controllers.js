007ce49312235f6a07d5ca84609cbe34e699cff1
Justin Levine <jclevine.receipts@gmail.com>
2013-10-02 18:07:32 -0400

    Properly use ng-model to set variable.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,10 +5,6 @@
 angular.module('BgScoreTally.controllers', []).
   controller("SelectionCtrl", ["$scope", "SelectionService", function($scope, SelectionService) {
     $scope.selections = SelectionService.selections;
-    $scope.updateNumPlayers = function(newNumPlayers) {
-      $scope.selectedNumPlayers = newNumPlayers;
-    }
-
     $scope.getSelectedNumPlayers = function() {
       return new Array(parseInt($scope.selectedNumPlayers));
     }
