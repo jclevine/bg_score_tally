4737678f313e68e1cd263b6f72d10ee03d89d58f
Justin Levine <jclevine.receipts@gmail.com>
2013-10-03 11:01:51 -0400

    Add level to selected game/num players for safety across controller scopes.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,9 +6,9 @@ angular.module('BgScoreTally.controllers', []).
   controller("SelectionCtrl", ["$scope", "SelectionService", function($scope, SelectionService) {
     $scope.selections = SelectionService.selections;
     $scope.getSelectedNumPlayers = function() {
-      return new Array(parseInt($scope.selectedNumPlayers));
+      return new Array(parseInt($scope.selected.num_players));
     }
     $scope.getNumOfScores = function() {
-      return new Array(parseInt($scope.selectedGame));
+      return new Array(parseInt($scope.selected.game));
     }
 }]);
