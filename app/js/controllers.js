ad9698b2dda4bb7933fb9c4f951dadbcd5a01e26
Justin Levine <jclevine.receipts@gmail.com>
2013-10-03 15:48:47 -0400

    Got total score calc working... weirdly. There's gotta be a better way.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,16 +2,49 @@
 
 /* Controllers */
 
-angular.module('BgScoreTally.controllers', []).
-  controller("SelectionCtrl", ["$scope", "SelectionService", function($scope, SelectionService) {
-    $scope.selections = SelectionService.selections;
-    $scope.getSelectedNumPlayers = function() {
-      return new Array(parseInt($scope.selected.num_players));
-    };
-    $scope.getScoreTypeNames = function() {
-      var selectedGame = $scope.selected.game;
-      if (selectedGame != -1) {
-        return $scope.selections.games[selectedGame].score_type_names;
-      }
-    };
+var bgScoreTally = angular.module('BgScoreTally.controllers', []);
+
+bgScoreTally.controller("SelectionCtrl", ["$scope", "SelectionService", function($scope, SelectionService) {
+  $scope.selections = SelectionService.selections;
+  $scope.getSelectedNumPlayers = function(currentRow) {
+    if ($scope.state == undefined) {
+      $scope.state = {};
+    }
+    $scope.state.currentRow = currentRow;
+    return new Array(parseInt($scope.selected.num_players));
+  };
+  $scope.getScoreTypeNames = function() {
+    var selectedGame = $scope.selected.game;
+    if (selectedGame != -1) {
+      return $scope.selections.games[selectedGame].score_type_names;
+    }
+  };
+
+  $scope.updateTotal = function() {
+    var newScore = this.score;
+
+    if (!$scope.scores) {
+      $scope.scores = {};
+    }
+    // var otherScoresOnRow = $scope.scores[this.$parent.$index];
+    if (!$scope.scores[this.$parent.$index]) {
+      $scope.scores[this.$parent.$index] = {};
+    }
+    $scope.scores[this.$parent.$index][this.$index] = parseInt(newScore);
+
+    if (!$scope.totalScores) {
+      $scope.totalScores = [];
+    }
+
+    var totalScore = 0;
+    for (var key in $scope.scores[this.$parent.$index]) {
+      totalScore += $scope.scores[this.$parent.$index][key];
+    }
+
+    $scope.totalScores[this.$parent.$index] = totalScore;
+  }
 }]);
+
+//bgScoreTally.controller("TotalCalcCtrl", ["$scope", function($scope) {
+//  alert($scope.state.currentRow);
+//}]);
