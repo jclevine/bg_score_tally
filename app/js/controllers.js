b18800076f84590ab15e3574abeebf6c02dd3bcc
Justin Levine <jclevine.receipts@gmail.com>
2013-10-04 21:37:43 -0400

    Possible simplification of model of score board. Add player doesn't work yet.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,13 +6,10 @@ var bgScoreTally = angular.module('BgScoreTally.controllers', []);
 
 bgScoreTally.controller("SelectionCtrl", ["$scope", "SelectionService", function($scope, SelectionService) {
   $scope.selections = SelectionService.selections;
-  $scope.getSelectedNumPlayers = function(currentRow) {
-    if ($scope.state == undefined) {
-      $scope.state = {};
-    }
-    $scope.state.currentRow = currentRow;
-    return new Array(parseInt($scope.selected.num_players));
-  };
+
+  $scope.playerScores = [];
+  $scope.totalScores = [];
+
   $scope.getScoreTypeNames = function() {
     var selectedGame = $scope.selected.game;
     if (selectedGame != -1) {
@@ -22,35 +19,35 @@ bgScoreTally.controller("SelectionCtrl", ["$scope", "SelectionService", function
 
   // Don't use any intermediate variables to point to $scope attributes because it doesn't work for whatever reason.
   $scope.updateTotal = function() {
-    var newScore = this.score;
-
-    if (!$scope.scores) {
-      $scope.scores = {};
-    }
-    if (!$scope.scores[this.$parent.$index]) {
-      $scope.scores[this.$parent.$index] = {};
-    }
-    $scope.scores[this.$parent.$index][this.$index] = parseInt(newScore);
-
-    if (!$scope.totalScores) {
-      $scope.totalScores = [];
-    }
-
     var totalScore = 0;
-    for (var key in $scope.scores[this.$parent.$index]) {
-      totalScore += $scope.scores[this.$parent.$index][key];
+    for (var i = 0; i < $scope.playerScores[this.$parent.$index].length; ++i) {
+      var score = $scope.playerScores[this.$parent.$index][i];
+      if (score != undefined) {
+        totalScore += parseInt(score);
+      }
     }
-
     $scope.totalScores[this.$parent.$index] = totalScore;
   }
 
-  $scope.resetTotalScores = function() {
-    $scope.scores = {};
-    $scope.totalScores = [];
+  $scope.reset = function() {
+    // TODO: jlevine - Make common function.
+    $scope.playerScores = new Array($scope.selected.numPlayers);
+    for (var i = 0; i < $scope.playerScores.length; ++i) {
+      $scope.playerScores[i] = new Array($scope.selections.games[$scope.selected.game].score_type_names.length)
+    }
   }
 
   $scope.addPlayer = function() {
     $scope.selected.num_players++;
   }
+
+  $scope.updatePlayerNumber = function() {
+    $scope.playerScores = new Array($scope.selected.numPlayers);
+    for (var i = 0; i < $scope.playerScores.length; ++i) {
+      $scope.playerScores[i] = new Array($scope.selections.games[$scope.selected.game].score_type_names.length);
+    }
+  }
+
+
 
 }]);
