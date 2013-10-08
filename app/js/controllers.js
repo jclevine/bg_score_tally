9dbc4c540d88608fc4189fc8f473f602550bba74
Andrew Wilson <aw65@nyu.edu>
2013-10-07 22:50:28 -0400

    Convert score_type_names to array of Objects, each with name and value (either a number or a function).
    Controller updateTotal uses the input to either:
    - multiply by the score object's value, if a number
    - input to the score object's value, if a function

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -58,12 +58,15 @@ bgScoreTally.controller("SelectionCtrl", ["$scope", "SelectionService", function
   // Don't use any intermediate variables to point to $scope attributes because it doesn't work for whatever reason.
   $scope.updateTotal = function(studentIndex) {
     var totalScore = 0;
-    for (var i = 0; i < $scope.playerScores[studentIndex].length; ++i) {
-      var score = $scope.playerScores[studentIndex][i];
-      if (score != undefined) {
-        totalScore += parseInt(score);
-      }
-    }
+	_.each($scope.selections.games[$scope.selected.game].score_type_names, function(scoreItem, index) {
+		var val = scoreItem.value,
+			amount = parseInt($scope.playerScores[studentIndex][index]),
+			result = 0;
+		if (typeof(amount) === 'number') {
+			result = typeof(val) === 'number' ? val * amount : val(amount);
+			totalScore += isNaN(result) ? 0 : result;
+		}
+    });
     $scope.totalScores[studentIndex] = isNaN(totalScore) ? 0 : totalScore;
   };
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -12,12 +12,197 @@ bgScoreTallyModule.factory("SelectionService", function() {
             {
                 "label": "Agricola",
                 "value": "agricola",
-                "score_type_names": ["Empty Spaces", "Sheep", "Hogs", "Cows", "House Material", "Card Points", "Pastures", "Fields"]
+                "score_type_names": [
+					{
+						name: "Fields",
+						value: function (amount) {
+							var val;
+							if (amount === 2 || amount === 3 || amount === 4) {
+								val = amount - 1;
+							} else if (5 <= amount) {
+								val = 4;
+							} else {
+								val = -1;
+							}
+							return val;
+						},
+					},
+					{
+						name: "Pastures",
+						value: function (amount) {
+							var val;
+							if (amount === 1 || amount === 2 || amount === 3) {
+								val = amount;
+							} else if (4 <= amount) {
+								val = 4;
+							} else {
+								val = -1;
+							}
+							return val;
+						},
+					},
+					{
+						name: "Grain",
+						value: function (amount) {
+							var val;
+							if (amount === 1 || amount === 2 || amount === 3) {
+								val = 1;
+							} else if (amount === 4 || amount === 5) {
+								val = 2;
+							} else if (amount === 6 || amount === 7) {
+								val = 3;
+							} else if (8 <= amount) {
+								val = 4;
+							} else {
+								val = -1;
+							}
+							return val;
+						},
+					},
+					{
+						name: "Vegetables",
+						value: function (amount) {
+							var val;
+							if (amount === 1 || amount === 2 || amount === 3) {
+								val = amount;
+							} else if (4 <= amount) {
+								val = 4;
+							} else {
+								val = -1;
+							}
+							return val;
+						},
+					},
+					{
+						name: "Sheep",
+						value: function (amount) {
+							var val;
+							if (amount === 1 || amount === 2 || amount === 3) {
+								val = 1;
+							} else if (amount === 4 || amount === 5) {
+								val = 2;
+							} else if (amount === 6 || amount === 7) {
+								val = 3;
+							} else if (8 <= amount) {
+								val = 4;
+							} else {
+								val = -1;
+							}
+							return val;
+						},
+					},
+					{
+						name: "Wild Boar",
+						value: function (amount) {
+							var val;
+							if (amount === 1 || amount === 2) {
+								val = 1;
+							} else if (amount === 3 || amount === 4) {
+								val = 2;
+							} else if (amount === 5 || amount === 6) {
+								val = 3;
+							} else if (7 <= amount) {
+								val = 4;
+							} else {
+								val = -1;
+							}
+							return val;
+						},
+					},
+					{
+						name: "Cattle",
+						value: function (amount) {
+							var val;
+							if (amount === 1) {
+								val = 1;
+							} else if (amount === 2 || amount === 3) {
+								val = 2;
+							} else if (amount === 4 || amount === 5) {
+								val = 3;
+							} else if (6 <= amount) {
+								val = 4;
+							} else {
+								val = -1;
+							}
+							return val;
+						},
+					},
+					{
+						name: "Empty Spaces",
+						value: -1,
+					},
+					{
+						// TODO: wilsona - configurable value function opts
+						name: "House Material",
+						value: function (amount, material) {
+							var val;
+							if (material === 'brick') {
+								val = amount;
+							} else if (material === 'stone') {
+								val = 2 * amount;
+							} else {
+								val = 0;
+							}
+							return val;
+						},
+					},
+					{
+						name: "Fenced Stables",
+						value: 1,
+					},
+					{
+						name: "Family Members",
+						value: 3,
+					},
+					{
+						name: "Card Points",
+						value: 1,
+					},
+					{
+						name: "Bonus Points",
+						value: 1,
+					},
+				]
             },
             {
                 "label": "7 Wonders",
                 "value": "7-wonders",
-                "score_type_names": ["Blue Cards", "Yellow Cards", "Wonder", "Military"]
+                "score_type_names": [
+					{
+						name: "Military",
+						value: 1,
+					},
+					{
+						name: "Coins",
+						value: function (amount) {
+							return Math.floor(amount / 3);
+						},
+					},
+					{
+						name: "Wonder",
+						value: 1,
+					},
+					{
+						name: "Blue Cards",
+						value: 1,
+					},
+					{
+						name: "Yellow Cards",
+						value: 1,
+					},
+					{
+						name: "Purple Cards",
+						value: 1,
+					},
+					{
+						name: "Green Cards",
+						value: function (a, b, c) {
+							// TODO: wilsona - Figure out implementation details.
+							//       requires configurable value function opts, above
+							return a;
+						},
+					},
+				]
             }
         ],
         "numPlayers": _.range(1, 11)
